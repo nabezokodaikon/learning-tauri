91e2bfccf0a550ca3d5eb9e8504d73ad43c8ea86
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-06-23 00:36:05 +0900

    Add Let's use Command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,57 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use serde::{Deserialize, Serialize};
+
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .run(context)
-    .expect("error while running tauri application");
+    // let context = tauri::generate_context!();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![async_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyMessage {
+    field_str: String,
+    field_u32: u32,
+}
+
+#[tauri::command]
+fn simple_command() {
+    println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn command_with_message(message: String) -> String {
+    format!("hello {}", message)
+}
+
+#[tauri::command]
+fn command_with_object(message: MyMessage) -> MyMessage {
+    let MyMessage {
+        field_str,
+        field_u32,
+    } = message;
+
+    MyMessage {
+        field_str: format!("hello {}", field_str),
+        field_u32: field_u32 + 1,
+    }
+}
+
+#[tauri::command]
+fn command_with_error(arg: u32) -> Result<String, String> {
+    if arg % 2 == 0 {
+        Ok(format!("even value {}", arg))
+    } else {
+        Err(format!("odd value {}", arg))
+    }
+}
+
+#[tauri::command]
+async fn async_command(arg: u32) -> String {
+    "hello".into()
 }
